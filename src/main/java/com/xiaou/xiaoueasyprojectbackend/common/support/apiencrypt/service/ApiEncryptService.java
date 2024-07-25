b03d847a9cca24b6fad82b176f1060fe99afdab5
aliyun1153623907 <3153566913@qq.com>
2024-07-25 16:39:54 +0800

    feat: 添加了api接口加密注解

---
+++ src/main/java/com/xiaou/xiaoueasyprojectbackend/common/support/apiencrypt/service/ApiEncryptService.java
@@ -0,0 +1,25 @@
+package com.xiaou.xiaoueasyprojectbackend.common.support.apiencrypt.service;
+
+/**
+ * 接口加密、解密 Service
+ *
+ */
+
+public interface ApiEncryptService {
+
+    /**
+     * 解密
+     * @param data
+     * @return
+     */
+    String decrypt(String data);
+
+    /**
+     * 加密
+     *
+     * @param data
+     * @return
+     */
+    String encrypt(String data);
+
+}
