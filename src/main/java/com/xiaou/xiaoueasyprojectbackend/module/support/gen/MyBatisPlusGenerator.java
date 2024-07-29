be0259c76f04a40a6b6c5cd2f651141aae4680f5
aliyun1153623907 <3153566913@qq.com>
2024-07-29 15:21:38 +0800

    feat: 添加了mybatis代码生成

---
+++ src/main/java/com/xiaou/xiaoueasyprojectbackend/module/support/gen/MyBatisPlusGenerator.java
@@ -0,0 +1,97 @@
+package com.xiaou;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+/**
+ * @Description
+ * @Version 1.0
+ **/
+public class MyBatisPlusGenerator {
+
+    public static void main(String[] args) {
+        //1. 全局配置
+        GlobalConfig config = new GlobalConfig();
+        // 是否支持AR模式
+        config.setActiveRecord(true)
+                // 作者
+                .setAuthor("xiaou")
+                // 生成路径，最好使用绝对路径
+                //TODO  TODO  TODO  TODO
+                .setOutputDir("C:\\Users\\Lenovo\\Desktop\\code")
+                // 文件覆盖
+                .setFileOverride(true)
+                // 主键策略
+                .setIdType(IdType.AUTO)
+
+                .setDateType(DateType.ONLY_DATE)
+                // 设置生成的service接口的名字的首字母是否为I，默认Service是以I开头的
+                .setServiceName("%sService")
+
+                //实体类结尾名称
+                .setEntityName("%sDO")
+
+                //生成基本的resultMap
+                .setBaseResultMap(true)
+
+                //不使用AR模式
+                .setActiveRecord(false)
+
+                //生成基本的SQL片段
+                .setBaseColumnList(true);
+
+        //2. 数据源配置
+        DataSourceConfig dsConfig = new DataSourceConfig();
+        // 设置数据库类型
+        dsConfig.setDbType(DbType.MYSQL)
+                .setDriverName("com.mysql.cj.jdbc.Driver")
+                //TODO  TODO  TODO  TODO
+                .setUrl("jdbc:mysql://localhost/dcloud_account?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=Asia/Shanghai")
+                .setUsername("root")
+                .setPassword("1234");
+
+        //3. 策略配置globalConfiguration中
+        StrategyConfig stConfig = new StrategyConfig();
+
+        //全局大写命名
+        stConfig.setCapitalMode(true)
+                // 数据库表映射到实体的命名策略
+                .setNaming(NamingStrategy.underline_to_camel)
+
+                //使用lombok
+                .setEntityLombokModel(true)
+
+                //使用restcontroller注解
+                .setRestControllerStyle(true)
+
+                // 生成的表, 支持多表一起生成，以数组形式填写
+                //TODO  TODO  TODO  TODO
+                .setInclude("account", "traffic", "traffic_task");
+
+        //4. 包名策略配置
+        PackageConfig pkConfig = new PackageConfig();
+        pkConfig.setParent("com.xiaou")
+                .setMapper("mapper")
+                .setService("service")
+                .setController("controller")
+                .setEntity("model")
+                .setXml("mapper");
+
+        //5. 整合配置
+        AutoGenerator ag = new AutoGenerator();
+        ag.setGlobalConfig(config)
+                .setDataSource(dsConfig)
+                .setStrategy(stConfig)
+                .setPackageInfo(pkConfig);
+        //6. 执行操作
+        ag.execute();
+        System.out.println("代码生成完毕");
+    }
+}
