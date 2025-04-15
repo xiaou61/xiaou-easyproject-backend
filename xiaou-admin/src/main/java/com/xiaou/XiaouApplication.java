06aaa21d84e075a746102ad1fe8dd3eded76e436
aliyun1153623907 <3153566913@qq.com>
2025-04-15 17:38:06 +0800

    init 框架

---
+++ xiaou-admin/src/main/java/com/xiaou/XiaouApplication.java
@@ -0,0 +1,12 @@
+package com.xiaou;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaouApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaouApplication.class, args);
+    }
+}
