7ddb3d5c4c4e5397d879b6a6e3c0716ce499bc84
aliyun1153623907 <3153566913@qq.com>
2025-04-15 20:30:18 +0800

    feat: 集成mybatis-plus + p6spy

---
+++ sql/v0.0.1.sql
@@ -0,0 +1,24 @@
+-- 删除已存在的数据库（使用反引号包裹带中划线的名字）
+DROP DATABASE IF EXISTS `sx-easy`;
+
+-- 创建新的数据库
+CREATE DATABASE `sx-easy` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+-- 使用该数据库
+USE `sx-easy`;
+
+-- 删除已存在的 sys_user 表
+DROP TABLE IF EXISTS sys_user;
+
+-- 创建 sys_user 用户表
+CREATE TABLE sys_user
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY COMMENT '用户ID',
+    name        VARCHAR(50)  NOT NULL UNIQUE COMMENT '用户名',
+    password    VARCHAR(100) NOT NULL COMMENT '用户密码',
+    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT ='用户表';
+INSERT INTO sys_user (name, password)
+VALUES ('xiaou', 'xiaou');
